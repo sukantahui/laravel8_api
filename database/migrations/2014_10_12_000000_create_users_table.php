6836977cc800fc6b5d838c24765cfddf5af9546d
sukanta hui <huisukanta77@gmail.com>
2020-11-24 22:42:09 +0530

    asfasf

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('id');
             $table->string('user_name')->nullable(true);
-            $table->string('email');
+            $table->string('email')->unique();
 
             $table->string('password');
             $table->rememberToken();
